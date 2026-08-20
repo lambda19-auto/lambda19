fccd866f8b764e8c7254eb867c13bab226bc614c
main <lidofgen@gmail.com>
2026-08-20 12:16:44 +0700

    fix: keep chat launcher aligned during close animation

--- src/components/ChatWidget.tsx
+++ src/components/ChatWidget.tsx
@@ -96,7 +96,7 @@ export default function ChatWidget() {
   };
 
   return (
-    <div className="fixed bottom-6 right-6 z-50">
+    <div className="fixed bottom-6 right-6 z-50 flex flex-col items-end">
       <AnimatePresence>
         {isOpen && (
           <motion.div
